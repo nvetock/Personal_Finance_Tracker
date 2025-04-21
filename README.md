1815de07caa1ce87459a25c32c2025484aa5aa70
straws <NVetock1@gmail.com>
2025-04-20 22:02:43 -0500

    Outlined loose structure in main()

---
+++ README.md
@@ -0,0 +1,10 @@
+# Personal Finance Tracker
+The goal of this project is to create a lightweight finance tracker that allows a user to easily input income and expenses.
+
+
+
+## Nice-to-haves
+- Persistent database
+- Budgeting capabilities
+- Planning tools
+- Some form of visualization
--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@ namespace SysInterface {
 	}
 
 	void printCommandMenu() {
-		std::cout << "[ Commands interface Here ]\n";
+		std::cout << "Command Menu\n"
+			<< "||||| (1)View | (2)Add Entry | (3)Quit |||||" << std::endl;
 	}
+
+
 }
 
 int main()
@@ -17,13 +20,20 @@ int main()
 	SysInterface::printMsg("Personal Finance Tracker is running...");
 	Logger myLogger("Logs/main.log");
 
-	/*
+	// Database class instantiates here
+	
 	while (true) {
-
 		SysInterface::printCommandMenu();
 
+		// Get menu option from user
+
+		// View asks to filter by date or type
+
+		// Add Entry asks if income or expense
+
+		// Quit
 	}
-	*/
+	
 
 	SysInterface::printMsg("Personal Finance Tracker shutting down");
 	return 0;
